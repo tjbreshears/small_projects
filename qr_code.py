09e5d92a68d2f711229fd3b2f9cd1730541a3aeb
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-02-01 18:51:00 -1000

    Change file save location to desktop
    Also added error correction level to QR creation, just for fun, and some clear comments.

--- qr_code.py
+++ qr_code.py
@@ -1,6 +1,6 @@
 import pyqrcode
 import png
-
+import os
 
 print("URL/text to make into a QR Code")
 text = input(": ")
@@ -9,8 +9,13 @@ print("Name your QR Code file")
 name = input(": ")
 full_name = name + ".png"
 
-# Creating QR code
-url=pyqrcode.create(text)
+# Create QR code
+url=pyqrcode.create(text, error = 'M')
 
+#Find current user's desktop and change directory.
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+os.chdir(desktop)
+
+#Make QR code png
 url.show()
 url.png(full_name, scale =6)
